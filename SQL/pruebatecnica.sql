bb1f658cf9f31ae735f96dd9791f9208801dd42e
94manuel <manu_fer_santo@live.com.mx>
2022-01-17 07:11:26 -0500

    Sql
    Sql

---
+++ SQL/pruebatecnica.sql
@@ -0,0 +1,67 @@
+/*
+SQLyog Ultimate v11.33 (64 bit)
+MySQL - 10.3.16-MariaDB : Database - pruebatecnica
+*********************************************************************
+*/
+
+/*!40101 SET NAMES utf8 */;
+
+/*!40101 SET SQL_MODE=''*/;
+
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+CREATE DATABASE /*!32312 IF NOT EXISTS*/`pruebatecnica` /*!40100 DEFAULT CHARACTER SET utf8 COLLATE utf8_spanish2_ci */;
+
+USE `pruebatecnica`;
+
+/*Table structure for table `computadores` */
+
+DROP TABLE IF EXISTS `computadores`;
+
+CREATE TABLE `computadores` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `pcfavoritos` varchar(255) COLLATE utf8_spanish2_ci DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=11 DEFAULT CHARSET=utf8 COLLATE=utf8_spanish2_ci;
+
+/*Data for the table `computadores` */
+
+insert  into `computadores`(`id`,`pcfavoritos`) values (1,'ASus'),(2,'Lenovo'),(3,'Apple'),(4,'Dell'),(5,'Acer'),(6,'Alienware'),(7,'Razer'),(8,'Microsoft'),(9,'MSI'),(10,'Samsung');
+
+/*Table structure for table `formulario` */
+
+DROP TABLE IF EXISTS `formulario`;
+
+CREATE TABLE `formulario` (
+  `id` int(11) NOT NULL,
+  `comentarios` varchar(300) COLLATE utf8_spanish2_ci DEFAULT NULL,
+  `pc_favorita` varchar(100) COLLATE utf8_spanish2_ci DEFAULT NULL,
+  `documento` varchar(10) COLLATE utf8_spanish2_ci DEFAULT NULL,
+  `email` varchar(100) COLLATE utf8_spanish2_ci DEFAULT NULL,
+  `fecha` varchar(50) COLLATE utf8_spanish2_ci DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_spanish2_ci;
+
+/*Data for the table `formulario` */
+
+/*Table structure for table `login` */
+
+DROP TABLE IF EXISTS `login`;
+
+CREATE TABLE `login` (
+  `id` int(11) NOT NULL,
+  `email` varchar(100) COLLATE utf8_spanish2_ci DEFAULT NULL,
+  `pass` varchar(100) COLLATE utf8_spanish2_ci DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_spanish2_ci;
+
+/*Data for the table `login` */
+
+insert  into `login`(`id`,`email`,`pass`) values (0,'ejemplo@ejemplo.com','12345678'),(2,'ejemplo@ejemplo.com','12345678');
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
